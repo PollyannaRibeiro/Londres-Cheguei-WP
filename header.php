8c810d9c59e6cb6a3ecf800fc48206eb39b64c52
Polly <23667108+PollyannaRibeiro@users.noreply.github.com>
2019-02-15 14:07:44 +0000

    Added header menu (pages, categories), font awesome link ant header css style

--- header.php
+++ header.php
@@ -11,6 +11,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
 			integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" 
+			integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+
 <link rel="stylesheet" href="<?= $home;?>/style.css">
 
 <?php wp_head(); ?>
@@ -23,6 +27,10 @@
 				<div class="row">
 					<div class="col-4 header-menu" id="logo-space">
 						<img src="<?= $home;?>/assets/images/logo/logo-londres-cheguei.png" id="logo" class="center"/>
+					</div>
+					<div class="col-5 header-menu" >
+						<div class="row" id="pages">Pages</div>
+						<div class="row" id="menu-categories">Categories</div>
 					</div>
 				</div>
 			</div>
