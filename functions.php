ca7bd6c9e8b1d313d34004a629bd7caa7c3eebf8
Polly <23667108+PollyannaRibeiro@users.noreply.github.com>
2019-02-21 13:36:47 +0000

    added thumbnails to the site

--- functions.php
+++ functions.php
@@ -6,3 +6,13 @@
 /**
  * Twenty Seventeen only works in WordPress 4.7 or later.
  */
+
+
+
+// Add thumbnails to the site
+
+function blog_setup() {
+	add_theme_support( 'post-thumbnails' );
+} 
+
+blog_setup();
