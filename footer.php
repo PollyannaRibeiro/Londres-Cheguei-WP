0b4e9a2d05c307d0ff3ef1d1f47a953262fa063c
Polly <23667108+PollyannaRibeiro@users.noreply.github.com>
2019-03-14 12:56:37 +0000

    added Footer and its style

--- footer.php
+++ footer.php
@@ -6,20 +6,37 @@
 
 <?php wp_footer(); ?>
 
-		<footer id="footer" class="site-footer row" role="contentinfo">
-				<div class="row"></div>
+	<footer id="footer" class="page-footer" role="contentinfo">
+			<div class="row">
+				<div class="col-3 col-footer">
+					<div class="float-right">
+						<?php wp_nav_menu(array(
+							'theme_location' => 'menu-footer', 
+						)); ?>
+					</div>	
+				</div>
+				<div class="col-3 col-footer">
+					<div class="float-right">
+						<?php wp_nav_menu(array(
+							'theme_location' => 'menu-footer', 
+						)); ?>
+					</div>	
+				</div>
 			</div>
-		</footer>
-	</div><!-- .site-content-contain -->		
+		</div>
+	</footer>	
 </div><!-- #page -->
 
 
 <?php $home=get_template_directory_uri();?>
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="<?= $home;?>/assets/js/index.js"></script>
+<script src="<?= $home;?>/assets/node_modules/bootstrap/js/src/util.js"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> -->
+
 
 </body>
 </html>
