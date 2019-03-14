100827b04efd8a37e141dbedc7ffe4977b3373bf
Polly <23667108+PollyannaRibeiro@users.noreply.github.com>
2019-03-14 12:50:44 +0000

    added single page and style

--- single.php
+++ single.php
@@ -1,43 +1,80 @@
 <?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
- */
+/*
+Template Name: Single
+*/
+?>
 
-get_header(); ?>
+<?php get_header();?>
 
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<main>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+<div class="row" id="single-page">
+	<div class="col-9" id="main-content-single-page">
 
-				get_template_part( 'template-parts/post/content', get_post_format() );
+	<article class="main-article" >
+		
+		<?php
+			if(have_posts()) {
+				while(have_posts()){
+					the_post();
+		?>
+			<div class="container">
+			
+				<section class="title-single-page">
+					<h1><?php the_title();?></h1>
+				</section>
+				
+				<div class="row">
+					<div class="col-6 thumb-single-page"><?php the_post_thumbnail(); ?></div>
+				</div>
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+				<span class="info-single-post">
+					<p><?php the_date();?></p>
+					<p><?php _e( 'Postado por: ', 'textdomain' ); the_author_posts_link();?></p>
+				</span>
+								
+				<section>
+					<div class="content-single-post">
+						<?php the_content();?> 
+					</div>
+				</section>
+				<!-- categories -->
+				<div class="row categories-single-post">
+					<?php _e( 'Categorias: ', 'textdomain' ); the_category( ', ' );?>
+				</div>
+				<!-- tags		 -->
+				<div class="row tags-single-post">
+					<?php _e( '', 'textdomain' );the_tags();?>	
+				</div>		
+			</div>
 
-				the_post_navigation( array(
-					'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
-					'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ) . '</span></span>',
-				) );
+		<?php																
+				}
+			}
+		?>
+	</article>
 
-			endwhile; // End of the loop.
-			?>
+	
+	
+	<div>
+		<?php if ( dynamic_sidebar('content_widgets') ) : else : endif; ?>
+	</div>
+<!-- Comments -->
+	<div class="comments-format"><?php comments_template(); ?>
+		</div>
+	</div>	
+	
+	<!-- Sidebar -->
+	<div class="col-3" id="single-page-side-content">
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-	<?php get_sidebar(); ?>
-</div><!-- .wrap -->
+	</div>
 
-<?php get_footer();
+</div>
+
+
+
+</main>
+
+
+
+<?php get_footer();?>
