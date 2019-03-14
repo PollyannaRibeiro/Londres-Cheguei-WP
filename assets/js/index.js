db6e80bb1e1aec33ddc38d40be2ae7301a078acd
Polly <23667108+PollyannaRibeiro@users.noreply.github.com>
2019-03-14 12:58:28 +0000

    added function addClass()

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,12 @@
 function addClass(){
 
-  let elements = $('.blog-thumb img').addClass('card-img-top');
+  $('.blog-thumb img').addClass('card-img-top img-fluid');
+  $('.thumb img').addClass('card-img');
+  $('.highlight-thumb img').addClass('card-img img-fluid');
+  $('.carousel-thumb img').addClass('d-block w-100');
+  $('body').scrollspy({ target: '#navbar-example' });
 
+  
 	
   
   console.log(elements);
