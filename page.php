b7b4eb6e3e44e512a7242475a13338d3839873fa
Polly <23667108+PollyannaRibeiro@users.noreply.github.com>
2019-06-21 17:00:54 +0100

    added page style

--- page.php
+++ page.php
@@ -1,41 +1,30 @@
 <?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
- */
+/*
+Template Name: Page
+*/
+?>
+<?php get_header();?>
+	<div class="main-wrap">
+		<main class="container-fluid" id="page" role="main">
+			<div class="secondary ">
+				<?php 
+					if( have_posts() ) { 
+							while( have_posts() ) {
+							the_post(); 
+					?>		
+					<div  class="page-content">
+							<h1><?php the_title(); ?></h1>
+							<p class="card-text"><?php the_content();?></p>
+					
+					</div>
+				<?php 
+						}
+					}
+				?>
+			</div>
+		</main> 
+	</div><!-- .wrap -->
+  </div><!-- #content -->
+</div><!-- .site-content-contain -->	
 
-get_header(); ?>
-
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/page/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-</div><!-- .wrap -->
-
-<?php get_footer();
+<?php get_footer();?>
